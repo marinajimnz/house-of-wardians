c114e393ce42b43b70219c3ef10bb05a099d50c5
Marina Jimenez <marinajimnz@outlook.com>
2025-05-28 18:55:31 +0200

    feat: added astro-icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
+import astroIcon from 'astro-icon';
 
 export default defineConfig({
-  // sin necesidad de configurar nada para Tailwind
+  integrations: [astroIcon()],
 });
