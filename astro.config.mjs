00528206bb81700168fac3634b32b38602469775
Marina Jimenez <marinajimnz@outlook.com>
2025-07-09 14:57:02 +0200

    feat: main structure en + es

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import astroIcon from 'astro-icon';
+import path from 'path';
 
 export default defineConfig({
   integrations: [astroIcon()],
+  alias: {
+    '@components': path.resolve('./src/components'),
+  },
 });
